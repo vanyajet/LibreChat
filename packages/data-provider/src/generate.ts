626fead40d297e5448835058e4d7ce6fe1eda955
vanyajet <vanyajet@gmail.com>
2024-09-26 01:55:24 +0400

    some fixes for data provider

--- packages/data-provider/src/generate.ts
+++ packages/data-provider/src/generate.ts
@@ -414,6 +414,7 @@ export function validateSettingDefinitions(settings: SettingsConfiguration): voi
 
     // Default columnSpan
     if (!setting.columnSpan) {
+      // @ts-ignore
       setting.columnSpan = Math.floor(columns / 2);
     }
 
